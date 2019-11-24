63bf22ac8ec38606b5c6a4a4748ab5fa7250f991
mohitgurnani1 <mohitgurnani1@gmail.com>
2019-11-24 17:14:55 -0500

    Add files via upload
    Fine tuned pipelines with parallel performance

--- florida_preprocessing_pipeline.py
+++ florida_preprocessing_pipeline.py
@@ -64,35 +64,20 @@ COUNTY=sys.argv[1]
 date_str = sys.argv[2]
 file_date = datetime.strptime(date_str, "%Y%m%d")
 
+
 # In[44]:
 
 
-COUPLES_SAVED_PATH= "couples/"+date_str+"/couples_"+date_str+".csv"
-COUPLES_SAVED_PATH_INDIV="couples/"+date_str+"/couples_"+COUNTY+"_"+date_str+".csv"
+SAVED_PATH_INDIV= "preprocessed/"+date_str+"/florida_processed_"+COUNTY+"_"+date_str+".csv"
+SAVED_PATH_AGGR= "preprocessed/"+date_str+"/florida_processed_"+date_str+".csv"
+
 
 # In[6]:
 
 
-def get_county_files():
-    registration_path = REGISTRATION_PATH.format(str(date_str))
-    county_files = []
-    for f in listdir(registration_path):
-        if isfile(registration_path + "/" + f):
-            county_files.append(registration_path + "/" + f)
-    return county_files
-
-
-# In[7]:
-
-
-# county_files = get_county_files()
-
-
-# In[8]:
-
 
 county_file_name = './'+COUNTY+'_'+date_str+'.txt'
-print(county_file_name)
+
 
 # In[9]:
 
@@ -157,16 +142,16 @@ selective_headers = [
 
 
 # In[10]:
-
-print("Reading CSV")
-
 global_df = pd.read_csv( REGISTRATION_PATH.format(date_str) + "/" + county_file_name, sep='\t', names=registration_file_headers, usecols=selective_headers)
+
+
+print("Read CSV")
 
 
 # In[11]:
 
-print("reading done")
-# global_df.isnull().sum()
+
+global_df.isnull().sum()
 
 
 # In[12]:
@@ -179,7 +164,7 @@ global_df = global_df.dropna(subset=req_cols)
 # In[13]:
 
 
-# global_df.isnull().sum()
+global_df.isnull().sum()
 
 
 # In[14]:
@@ -196,10 +181,8 @@ def calculate_age(born):
     today = file_date.date()
     return today.year - born.year - ((today.month, today.day) < (born.month, born.day))
 
-print("Calculating age")
+print("Age calculation")
 global_df['age'] = global_df['birth_date'].apply(calculate_age)
-
-print("Done calculation age")
 
 # In[18]:
 
@@ -221,7 +204,7 @@ str_cols_upper = [
 
 # In[19]:
 
-print("Handling casing issues")
+
 for col in str_cols_lower:
     global_df[col] = global_df[col].apply(lambda x: str(x).strip().lower())
 for col in str_cols_upper:
@@ -230,7 +213,7 @@ for col in str_cols_upper:
 
 # In[20]:
 
-print("Handling zipcode 5 issues")
+
 global_df['residence_zipcode_5'] = global_df['residence_zipcode'].apply(lambda x: int(str(x)[:5]))
 
 
@@ -244,20 +227,17 @@ def generate_zipcode_4(zip):
     else:
         return ''
 
-print("Handling zipcode 4 issues")
 global_df['residence_zipcode_4'] = global_df['residence_zipcode'].apply(generate_zipcode_4)
 
 
 # In[22]:
 
 
-print("Generating unique addresses")
 global_df['uniq_addr'] = global_df[['residence_addr_line_1', 'residence_addr_line_2', 'residence_city', 'residence_zipcode_5']].apply(lambda x: ' '.join([str(y) for y in x]), axis=1)
 
 
 # In[23]:
 
-print("Removing alphanumeric chars from unique addresses")
 
 import re
 global_df['uniq_addr'] = global_df['uniq_addr'].apply(lambda x: re.sub("[^0-9a-zA-Z\s]+", '', x))
@@ -265,138 +245,13 @@ global_df['uniq_addr'] = global_df['uniq_addr'].apply(lambda x: re.sub("[^0-9a-z
 
 # In[24]:
 
-print("Removing leading/trailing white-spaces")
+
 global_df['uniq_addr'] = global_df['uniq_addr'].apply(lambda x: x.strip())
 
-
-# In[26]:
-
-
-# In[28]:
-
-print("Generating deepcopy: total rows " + str(global_df.shape))
-global_df_copy = global_df.copy(deep=True)
-
-
-# In[29]:
-
-print("Performing self join")
-merge = pd.merge(global_df, global_df_copy, on=["uniq_addr"], suffixes=["_L", "_R"])
-
-
-# In[30]:
-
-print("Considering unequal voter-id combinations")
-merge = merge[merge["voter_id_L"] != merge["voter_id_R"]]
-
-
-# In[31]:
-
-print("Rows after merge: " + str(merge.shape))
-
-# In[32]:
-
-
-filtered = merge[merge["voter_id_L"] < merge["voter_id_R"]]
-
-
-# In[33]:
-
-
-print("Filtering done: rows: " + str(filtered.shape))
-
-
-# In[34]:
-
-
-def modified_couple_heuristic(row):
-    male_age_threshold = 27
-    female_age_threshold = 25
-    unknown_age_threshold = 26
-    age_diff_threshold = 15
-    
-    age_diff = abs(row['age_L'] - row['age_R'])
-    
-    is_age_threshold_L = False
-    if row["gender_L"] == "M" and row["age_L"] >= male_age_threshold: 
-        is_age_threshold_L = True
-    elif row["gender_L"] == "F" and row["age_L"] >= female_age_threshold:
-        is_age_threshold_L = True
-    elif row["gender_L"] == "U" and row["age_L"] >= unknown_age_threshold:
-        is_age_threshold_L = True
-
-    
-        
-    is_age_threshold_R = False
-    if row["gender_R"] == "M" and row["age_R"] >= male_age_threshold: 
-        is_age_threshold_R = True
-    elif row["gender_R"] == "F" and row["age_R"] >= female_age_threshold:
-        is_age_threshold_R = True
-    elif row["gender_R"] == "U" and row["age_R"] >= unknown_age_threshold:
-        is_age_threshold_R = True
-    
-    return is_age_threshold_L and is_age_threshold_R and age_diff <= age_diff_threshold
-        
-
-
-# In[55]:
-
-couples = filtered[filtered.apply(modified_couple_heuristic, axis=1)]
-print("Couples found: " + str(couples.shape))
-
-# In[38]:
-
-
-from multiprocessing import  Pool
-def parallelize_dataframe(df, func, n_cores=4):
-    df_split = np.array_split(df, n_cores)
-    pool = Pool(n_cores)
-    df = pd.concat(pool.map(func, df_split))
-    pool.close()
-    pool.join()
-    return df
-
-
-# In[39]:
-
-
-#couples = parallelize_dataframe(filtered, modified_couple_heuristic, 8)
-
-
-# In[56]:
-
-print('Couples processed so far')
-
-print(couples.shape)
-
-
-# #### https://datascience.stackexchange.com/questions/26308/after-grouping-to-minimum-value-in-pandas-how-to-display-the-matching-row-resul
-# #### https://jamesrledoux.com/code/drop_duplicates
-
-# In[76]:
-
-
-print("Computing age diff")
-couples["age_diff"] = couples.apply(lambda row: abs(row["age_L"] - row["age_R"]), axis=1)
-
-
-# In[78]:
-
-
-sorted_couples = couples.sort_values(by="age_diff")
-
-
-# In[82]:
-
-
-single_house_couples = sorted_couples.drop_duplicates(subset="uniq_addr", keep="first")
-print("Single house couples found: " + str(single_house_couples.shape))
-
-# In[ ]:
+print("Saving file")
 
 
 def appendDFToCSV(df, csvFilePath, sep="\t"):
-    print("appendDFToCSV: " + csvFilePath)
     import os
     if not os.path.isfile(csvFilePath):
         df.to_csv(csvFilePath, mode='a', index=False, sep=sep)
@@ -408,8 +263,5 @@ def appendDFToCSV(df, csvFilePath, sep="\t"):
         df.to_csv(csvFilePath, mode='a', index=False, sep=sep, header=False)
 
 
-# In[ ]:
-
-
-# appendDFToCSV(single_house_couples, COUPLES_SAVED_PATH)
-appendDFToCSV(single_house_couples, COUPLES_SAVED_PATH_INDIV)
+appendDFToCSV(global_df, SAVED_PATH_INDIV)
+appendDFToCSV(global_df, SAVED_PATH_AGGR)
---
+++ script_couples.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+date=$1
+echo $date
+vd="${date}_VoterDetail"
+echo $vd
+
+if [ ! -d couples/$date ]; then
+  mkdir -p couples/$date;
+fi
+
+for file in data/$vd/*; do
+	f="$(basename "$file")"
+	county="$(echo $f  | cut -d'_' -f1)"
+	python florida_couples_pipeline.py $county $date
+done
